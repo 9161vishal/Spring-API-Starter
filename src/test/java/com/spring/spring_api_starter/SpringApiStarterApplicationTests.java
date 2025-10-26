799411029a0b7187dd82fca4768b53004782baf7
Vishal Chaudhary <9161vishal@gmail.com>
2025-10-26 13:00:55 +0530

    First Commit

---
+++ src/test/java/com/spring/spring_api_starter/SpringApiStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_api_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
